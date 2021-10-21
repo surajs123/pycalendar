e18a514c069ccea8398644fcc737d9d9326d323b
suraj surendran <surajsuraj7600@gmail.com>
2021-10-21 18:13:59 +0530

    modify the files and create the test.pipy account and upload the py_calendar pkg

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r",encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="py_calendar",                     # This is the name of the package
     version="0.0.1",                        # The initial release version
-    author="Suraj S",                     # Full name of the author
+    author="Suraj S", 
+    author_email="www.surajsuraj7600@gmail.com",                   
     description="To print the calendar design on the terminal ",
-    long_description=long_description,      # Long description read from the the readme file
+    long_description=long_description,   
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
-    zip_safe =False,
+    url= "https://github.com/surajs123/pycalendar",
+    packages=setuptools.find_packages(where="src"),   
+    project_url={
+        "Bug Tracker":"https://github.com/surajs123/pycalendar/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["py_calendar"],             # Name of the python package
+    ],                                      
+    python_requires='>=3.6',               
+    package_dir={"":"src"},           
    
    
 )
