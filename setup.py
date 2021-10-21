289517ff7a4ded5d47b033896f1bf4676b311558
suraj surendran <surajsuraj7600@gmail.com>
2021-10-21 16:54:58 +0530

    create new folder and make some changes in the package

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ setuptools.setup(
     author="Suraj S",                     # Full name of the author
     description="To print the calendar design on the terminal ",
     long_description=long_description,      # Long description read from the the readme file
-    long_description_content_type="markdown",
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),    # List of all python modules to be installed
+    zip_safe =False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -18,6 +19,6 @@ setuptools.setup(
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
     py_modules=["py_calendar"],             # Name of the python package
-    package_dir={'':'py_calendar'},     # Directory of the source code of the package
-    install_requires=[]                     # Install other dependencies if any
+   
+   
 )
